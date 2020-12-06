55cee63ace4c56ac04282231f4eb744f810ce37e
h4570 <hastoklai@gmail.com>
2020-12-06 20:20:08 +0100

    fixed path3 lighting on real ps2

--- src/engine/include/modules/light.hpp
+++ src/engine/include/modules/light.hpp
@@ -24,7 +24,7 @@ public:
     ~Light();
 
     static u16 getLightsCount(u32 t_bulbsCount);
-    static void calculateLight(VECTOR *t_lightDirections, VECTOR *t_lightColors, int *t_lightTypes, LightBulb *t_bulbs, u32 t_bulbsCount, Vector3 t_objPosition);
+    static void calculateLight(VECTOR *t_lightDirections, VECTOR *t_lightColors, int *t_lightTypes, LightBulb *t_bulbs, u32 t_lightsCount, Vector3 t_objPosition);
 
 private:
 };
