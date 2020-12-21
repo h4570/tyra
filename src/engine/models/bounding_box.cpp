02d79169255638f0c1ddf341981b951626542c11
Foxar <mostek3pl@gmail.com>
2020-12-21 19:04:34 +0100

    Fixed: Fixed incorrect naming of boundingbox constructor parameters.

--- src/engine/models/bounding_box.cpp
+++ src/engine/models/bounding_box.cpp
@@ -11,11 +11,11 @@
 #include "../include/models/bounding_box.hpp"
 #include "../include/utils/debug.hpp"
 
-BoundingBox::BoundingBox(Vector3 *t_vectorArray)
+BoundingBox::BoundingBox(Vector3 *t_vertices)
 {
     //Perform a deep copy of vertex array parameter
     for (int i = 0; i < 8; i++)
-        _vertices[i] = t_vectorArray[i];
+        _vertices[i] = t_vertices[i];
 
     //This might be shortened with Vector3 operator overloading, but current
     //implementation is more human readable.
