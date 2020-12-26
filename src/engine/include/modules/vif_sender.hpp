8406dcee72d096b4e6daf96543a2a5689bc47d2d
h4570 <hastoklai@gmail.com>
2020-12-26 13:29:37 +0100

    added optimized array mode to draw()

--- src/engine/include/modules/vif_sender.hpp
+++ src/engine/include/modules/vif_sender.hpp
@@ -31,12 +31,17 @@ public:
     // TODO refactor
     void drawMesh(RenderData *t_renderData, Matrix t_perspective, u32 vertCount2, VECTOR *vertices, VECTOR *normals, VECTOR *coordinates, Mesh &t_mesh, LightBulb *t_bulbs, u16 t_bulbsCount, texbuffer_t *textureBuffer);
     void calcMatrix(const RenderData &t_renderData, const Vector3 &t_position, const Vector3 &t_rotation);
+    void drawTheSameWithOtherMatrices(const RenderData &t_renderData, Mesh **t_meshes, const u32 &t_skip, const u32 &t_count);
+    void enableWait() { isDrawWaitEnabled = true; }
+    void disableWait() { isDrawWaitEnabled = false; }
 
 private:
+    u32 lastVertCount; // needed for drawTheSameWithOtherMatrices()
+    u8 isDrawWaitEnabled;
     Light *light;
     void uploadMicroProgram();
-    void setDoubleBuffer();
-    void drawVertices(Mesh &t_mesh, u32 t_start, u32 t_end, VECTOR *t_vertices, VECTOR *t_coordinates, prim_t *t_prim, texbuffer_t *textureBuffer);
+    void setDoubleBufferAddStaticData();
+    void drawVertices(Mesh &t_mesh, u32 t_start, u32 t_end, VECTOR *t_vertices, VECTOR *t_coordinates, prim_t *t_prim, texbuffer_t *t_textureBuffer, u8 t_addDrawWait);
     packet2_t *packets[2] __attribute__((aligned(64)));
     packet2_t *currPacket;
     /** 
