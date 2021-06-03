4a099f2fdf1407514b86e119607428b0ff0cc3b3
h4570 <sandro.sobczynski@gmail.com>
2021-06-03 18:52:50 +0200

    commented some future features
    Signed-off-by: h4570 <sandro.sobczynski@gmail.com>

--- src/samples/rocket_league/rocket_league.cpp
+++ src/samples/rocket_league/rocket_league.cpp
@@ -34,15 +34,15 @@ void RocketLeague::onInit()
     texRepo = engine->renderer->getTextureRepository();
     setBgColorAndAmbientColor();
     engine->renderer->setCameraDefinitions(&camera.view, &camera.unitCirclePosition, camera.planes);
-    // engine->audio.loadSong("sounds/long-night-1a.wav");
+    engine->audio.loadSong("sounds/long-night-1a.wav");
     player = new Player(texRepo);
     raceTrack = new RaceTrack(texRepo);
     beachBall = new BeachBall(texRepo);
     // ammoBox = new AmmoBox(texRepo);
-    policeCar = new PoliceCar(texRepo);
-    // skybox = new Skybox(texRepo);
-    // engine->audio.playSong();
-    // engine->audio.setSongVolume(80);
+    // policeCar = new PoliceCar(texRepo);
+    skybox = new Skybox(texRepo);
+    engine->audio.playSong();
+    engine->audio.setSongVolume(80);
 
     // TODO1: Przesunac bounding box do mesha
     // TODO2: Dodac update() przez pozycje i rotacje
@@ -54,7 +54,6 @@ void RocketLeague::onInit()
 void RocketLeague::onUpdate()
 {
     player->update(engine->pad);
-    // policeCar->update(engine->pad);
     camera.update(engine->pad, player->mesh);
 
     if (player->mesh.getCurrentBoundingBox()
@@ -64,12 +63,12 @@ void RocketLeague::onUpdate()
         beachBall->push(player->mesh.position, player->getAbsAcceleration() + 0.1F);
 
     beachBall->update();
-    // skybox->update(player->mesh.position);
+    skybox->update(player->mesh.position);
 
-    // engine->renderer->draw(skybox->mesh);
+    engine->renderer->draw(skybox->mesh);
     engine->renderer->draw(raceTrack->mesh);
     engine->renderer->draw(player->mesh);
-    engine->renderer->draw(policeCar->mesh);
+    // engine->renderer->draw(policeCar->mesh);
     // engine->renderer->draw(ammoBox->mesh);
     engine->renderer->draw(beachBall->mesh);
 }
@@ -95,9 +94,9 @@ void RocketLeague::pushBall()
         beachBall->push(player->mesh.position, player->getAbsAcceleration() + 0.1F);
 
     // Computer (police car)
-    if (policeCar->mesh.getCurrentBoundingBox()
-            ->intersect(
-                *beachBall->mesh.getCurrentBoundingBox(),
-                policeCar->mesh.position, beachBall->mesh.position))
-        beachBall->push(policeCar->mesh.position, policeCar->getAbsAcceleration() + 0.1F);
+    // if (policeCar->mesh.getCurrentBoundingBox()
+    //         ->intersect(
+    //             *beachBall->mesh.getCurrentBoundingBox(),
+    //             policeCar->mesh.position, beachBall->mesh.position))
+    //     beachBall->push(policeCar->mesh.position, policeCar->getAbsAcceleration() + 0.1F);
 }
