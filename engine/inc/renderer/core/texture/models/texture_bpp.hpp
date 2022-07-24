819a7c3de35561b852ccb9c26027a8a7cf3ad7dd
h4570 <sandro.sobczynski@gmail.com>
2022-07-24 23:08:10 +0200

    documentation update

--- engine/inc/renderer/core/texture/models/texture_bpp.hpp
+++ engine/inc/renderer/core/texture/models/texture_bpp.hpp
@@ -14,29 +14,29 @@ namespace Tyra {
 
 enum TextureBpp {
 
-  // Tyra has 1.3MB~ free VRAM for textures.
+  // Tyra has 2.27MB~ free VRAM for textures.
 
   /**
    * @brief 32-bit RGBA - Slowest
-   * Tyra can cache about 4~ of these textures (256x256)
+   * Tyra can cache about 8~ of these textures (256x256)
    */
   bpp32 = 32,
 
   /**
    * @brief 24-bit RGB - Slow
-   * Tyra can cache about 6~ of these textures (256x256)
+   * Tyra can cache about 11~ of these textures (256x256)
    */
   bpp24 = 24,
 
   /**
    * @brief 8-bit palletized (indexed) - Super fast
-   * Tyra can cache about 19~ of these textures (256x256)
+   * Tyra can cache about 34~ of these textures (256x256)
    */
   bpp8 = 8,
 
   /**
    * @brief 4-bit palletized (indexed) - Super, super fast
-   * Tyra can cache about 39~ of these textures (256x256)
+   * Tyra can cache about 69~ of these textures (256x256)
    */
   bpp4 = 4,
 };
