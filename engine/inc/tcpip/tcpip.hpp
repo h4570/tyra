39720c2f6c994a88538091f0706e12c31b8f26ee
Br4k2n <109167964+Br4k2n@users.noreply.github.com>
2022-11-03 12:04:50 -0300

    Update tcpip.hpp

--- engine/inc/tcpip/tcpip.hpp
+++ engine/inc/tcpip/tcpip.hpp
@@ -6,9 +6,6 @@
 #pragma once
 
 namespace Tyra {
-static char ipAddress[16];
-t_ip_info ip_info;
-const ip_addr_t *dns_curr;
 
 class TCP
 {
@@ -20,7 +17,7 @@ char *GetIP();
 int Init(int use_dhcp, ip4_addr *IP_adrr, ip4_addr *Netmask, ip4_addr *Gateway, ip4_addr *DNS);
 int End();
 int SetNetIFMode(int mode);
-static void PrintIPConfig();
+void PrintIPConfig();
 int DHCPCheck();
 
 };
