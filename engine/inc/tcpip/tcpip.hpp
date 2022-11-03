61d0626aed37ed263659c3ebf24a194967f6f2b1
Br4k2n <109167964+Br4k2n@users.noreply.github.com>
2022-11-02 21:51:07 -0300

    Update tcpip.hpp

--- engine/inc/tcpip/tcpip.hpp
+++ engine/inc/tcpip/tcpip.hpp
@@ -1,3 +1,8 @@
+#include <kernel.h>
+#include <netman.h>
+#include <ps2ips.h>
+#include <stdio.h>
+
 #pragma once
 
 namespace Tyra {
@@ -8,6 +13,9 @@ const ip_addr_t *dns_curr;
 class TCP
 {
 public:
+TCP();
+~TCP();
+
 char *GetIP();
 int Init(int use_dhcp, ip4_addr *IP_adrr, ip4_addr *Netmask, ip4_addr *Gateway, ip4_addr *DNS);
 int End();
