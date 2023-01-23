481ccd0cf954638bc1bc9f61851777431315a1ea
Wellinator <wellcoj@gmail.com>
2023-01-23 10:19:18 -0300

    feat: load fileXio and iomanX iop drivers

--- engine/src/irx/irx_loader.cpp
+++ engine/src/irx/irx_loader.cpp
@@ -33,6 +33,12 @@ extern int size_audsrv_irx;
 extern u8 libsd_irx[];
 extern int size_libsd_irx;
 
+extern u8 fileXio_irx_irx[];
+extern int size_fileXio_irx_irx;
+
+extern u8 iomanX_irx_irx[];
+extern int size_iomanX_irx_irx;
+
 extern u8 bdm_irx[];
 extern int size_bdm_irx;
 
@@ -114,6 +120,24 @@ void IrxLoader::loadLibsd(const bool& verbose) {
   TYRA_ASSERT(ret >= 0, "Failed to load module: libsd_irx");
 
   if (verbose) TYRA_LOG("IRX: Libsd loaded!");
+}
+
+void IrxLoader::loadPOSIX(const bool& verbose) {
+  if (verbose) TYRA_LOG("IRX: Loading fileXio...");
+
+  int ret;
+  SifExecModuleBuffer(&fileXio_irx, size_fileXio_irx, 0, nullptr, &ret);
+  TYRA_ASSERT(ret >= 0, "Failed to load module: fileXio_irx");
+
+  if (verbose) TYRA_LOG("IRX: fileXio_irx loaded!");
+
+  if (verbose) TYRA_LOG("IRX: Loading iomanX...");
+
+  int ret;
+  SifExecModuleBuffer(&iomanX_irx, size_iomanX_irx, 0, nullptr, &ret);
+  TYRA_ASSERT(ret >= 0, "Failed to load module: iomanX_irx");
+
+  if (verbose) TYRA_LOG("IRX: iomanX loaded!");
 }
 
 void IrxLoader::loadUsbModules(const bool& verbose) {
