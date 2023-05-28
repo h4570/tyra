20d2291a88e9e83739e7f0afaffa4ffbfe4523a8
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2023-05-28 16:46:32 -0300

    Update Tyra copyright

--- tutorials/10-sprite-sheet/inc/tutorial_10.hpp
+++ tutorials/10-sprite-sheet/inc/tutorial_10.hpp
@@ -3,8 +3,9 @@
 #   |     \/   ____| |___|
 #   |     |   |   \  |   |
 #-----------------------------------------------------------------------
-# Copyright 2022, tyra - https://github.com/h4570/tyra
+# Copyright 2022-2023, tyra - https://github.com/h4570/tyra
 # Licensed under Apache License 2.0
+# Guido Diego Quispe Robles
 # Sandro Sobczyński <sandro.sobczynski@gmail.com>
 */
 
--- tutorials/10-sprite-sheet/src/fontSprite.cpp
+++ tutorials/10-sprite-sheet/src/fontSprite.cpp
@@ -1,3 +1,14 @@
+/*
+# _____        ____   ___
+#   |     \/   ____| |___|
+#   |     |   |   \  |   |
+#-----------------------------------------------------------------------
+# Copyright 2022-2023, tyra - https://github.com/h4570/tyra
+# Licensed under Apache License 2.0
+# Guido Diego Quispe Robles
+# Sandro Sobczyński <sandro.sobczynski@gmail.com>
+*/
+
 #include "fontSprite.hpp"
 
 namespace Tyra{
