4c8f589dd0e3e7b6e66746900b73e17b0751e859
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2023-06-07 08:50:46 -0300

    Update banner.cpp

--- engine/src/info/banner.cpp
+++ engine/src/info/banner.cpp
@@ -3,9 +3,10 @@
 #   |     \/   ____| |___|
 #   |     |   |   \  |   |
 #-----------------------------------------------------------------------
-# Copyright 2022-2022, tyra - https://github.com/h4570/tyrav2
+# Copyright 2022-2023, tyra - https://github.com/h4570/tyrav2
 # Licensed under Apache License 2.0
 # Sandro Sobczyński <sandro.sobczynski@gmail.com>
+# André Guilherme <andregui17@outlook.com>
 */
 
 #include "info/banner.hpp"
